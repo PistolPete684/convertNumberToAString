044e384551a542845bf01b3ffe1930b67772e11f
Peter <pwlinton@mta.ca>
2023-06-26 22:51:56 -0300

    adding js file

---
+++ app.js
@@ -0,0 +1,5 @@
+//return the number passed through as an argument as a string
+//toString( ) is a built in method that return a number as a string
+function numberToString(num) {
+  return num.toString();
+}
